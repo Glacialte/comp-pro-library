bc39e981104f00444dc2f2727140872c4411d549
Glacialte <pikutaro25@gmail.com>
2026-01-02 12:08:23 +0000

    Rename methods of UnionFind and fix tests' name

--- data-structure/unionfind.hpp
+++ data-structure/unionfind.hpp
@@ -9,6 +9,9 @@ namespace gcl
     class UnionFind
     {
     public:
+        /*
+            @brief n 要素のUnionFindを構築する
+        */
         explicit UnionFind(std::size_t size) : _size(size, std::size_t{1}), _parent(size)
         {
 
@@ -16,31 +19,31 @@ namespace gcl
         }
 
         // `id`の所属する集合の代表元を返す
-        [[nodiscard]] std::size_t get_root(std::size_t id)
+        [[nodiscard]] std::size_t find(std::size_t id)
         {
             if (_parent[id] == id)
                 return id;
-            return _parent[id] = get_root(_parent[id]);
+            return _parent[id] = find(_parent[id]);
         }
 
         // `id`の所属する集合のサイズを返す
-        [[nodiscard]] std::size_t get_size(std::size_t id)
+        [[nodiscard]] std::size_t size(std::size_t id)
         {
-            return _size[get_root(id)];
+            return _size[find(id)];
         }
 
         // `left`と`right`の所属する集合が等しいか返す
-        [[nodiscard]] bool is_same(std::size_t left, std::size_t right)
+        [[nodiscard]] bool same(std::size_t left, std::size_t right)
         {
-            return get_root(left) == get_root(right);
+            return find(left) == find(right);
         }
 
         // `left`と`right`の所属する集合を結合する
         // サイズが同じ集合同士は左に結合
         bool unite(std::size_t left, std::size_t right)
         {
-            auto root_left = get_root(left);
-            auto root_right = get_root(right);
+            auto root_left = find(left);
+            auto root_right = find(right);
             if (root_left == root_right)
                 return false;
             auto size_left = _size[root_left];
--- tests/test_unionfind.cpp
+++ tests/test_unionfind.cpp
@@ -13,26 +13,56 @@ TEST(UnionFind, Unite)
     EXPECT_EQ(uf.unite(4, 4), false); // {0, 1, 2, 3}, {4}
 }
 
-TEST(UnionFind, GetSize)
+TEST(UnionFind, Size)
 {
     const auto size = std::size_t{3};
     gcl::UnionFind uf(size);
 
-    EXPECT_EQ(uf.get_size(0), 1);
+    EXPECT_EQ(uf.size(0), 1);
     uf.unite(0, 1);
     uf.unite(0, 2);
-    EXPECT_EQ(uf.get_size(0), 3);
+    EXPECT_EQ(uf.size(0), 3);
 }
 
-TEST(UnionFind, GetRoot)
+TEST(UnionFind, Find)
 {
     const auto size = std::size_t{3};
     gcl::UnionFind uf(size);
 
-    EXPECT_EQ(uf.get_root(0), 0);
+    EXPECT_EQ(uf.find(0), 0);
     uf.unite(0, 1); // {0, 1} root -> 0
     uf.unite(0, 2);
-    EXPECT_EQ(uf.get_root(0), 0);
-    EXPECT_EQ(uf.get_root(1), 0);
-    EXPECT_EQ(uf.get_root(2), 0);
+    EXPECT_EQ(uf.find(0), 0);
+    EXPECT_EQ(uf.find(1), 0);
+    EXPECT_EQ(uf.find(2), 0);
+}
+
+TEST(UnionFind, Same)
+{
+    const auto size = std::size_t{5};
+    gcl::UnionFind uf(size);
+
+    // 初期状態
+    EXPECT_EQ(uf.same(0, 0), true);
+    EXPECT_EQ(uf.same(0, 1), false);
+    EXPECT_EQ(uf.same(3, 4), false);
+
+    // unite 後
+    uf.unite(0, 1); // {0,1}
+    EXPECT_EQ(uf.same(0, 1), true);
+    EXPECT_EQ(uf.same(1, 0), true);
+    EXPECT_EQ(uf.same(0, 2), false);
+
+    uf.unite(2, 3); // {2,3}
+    EXPECT_EQ(uf.same(2, 3), true);
+    EXPECT_EQ(uf.same(1, 2), false);
+
+    // 推移的結合
+    uf.unite(1, 2); // {0,1,2,3}
+    EXPECT_EQ(uf.same(0, 3), true);
+    EXPECT_EQ(uf.same(3, 0), true);
+
+    // 孤立点
+    EXPECT_EQ(uf.same(0, 4), false);
+    EXPECT_EQ(uf.same(4, 4), true);
 }
